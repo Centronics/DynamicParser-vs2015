4746579b5c98b93c8876a274cb9ef19a1b21c583
Илья <kras-05@mail.ru>
2016-10-11 22:43:40 +0300

    Разработан класс контекстной линии.

--- DynamicParser/ContextLine.cs
+++ DynamicParser/ContextLine.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using DynamicProcessor;
 
 namespace DynamicParser
@@ -13,11 +11,33 @@ namespace DynamicParser
         public SignValue Sign;
     }
 
+    struct Line
+    {
+        public Points PtX, PtY;
+
+        public Line(Points ptx, Points pty)
+        {
+            if (ptx.Pt.Y != pty.Pt.Y)
+                if (ptx.Pt.X != pty.Pt.X)
+                    throw new Exception();
+            if (ptx.Pt.X != pty.Pt.X)
+                if (ptx.Pt.Y != pty.Pt.Y)
+                    throw new Exception();
+            PtX = ptx;
+            PtY = pty;
+        }
+
+        public int Length => PtX.Pt.Y == PtY.Pt.Y
+            ? (PtX.Pt.X > PtY.Pt.X ? PtX.Pt.X - PtY.Pt.X : PtY.Pt.X - PtX.Pt.X)
+            : (PtX.Pt.Y > PtY.Pt.Y ? PtX.Pt.Y - PtY.Pt.Y : PtY.Pt.Y - PtX.Pt.Y);
+    }
+
     class ContextLine
     {
         readonly List<Points> _points = new List<Points>();
         public int Width { get; }
         public int Height { get; }
+        int _lastY = -1, _lastX = -1;
 
         public ContextLine(Bitmap btm)
         {
@@ -29,7 +49,6 @@ namespace DynamicParser
             Height = btm.Height;
             for (int y = 0; y < btm.Height; y++)
                 for (int x = 0; x < btm.Width; x++)
-                {
                     _points.Add(new Points
                     {
                         Sign = new SignValue(btm.GetPixel(x, y)),
@@ -39,7 +58,6 @@ namespace DynamicParser
                             Y = y
                         }
                     });
-                }
         }
 
         public Points GetPixel(int x, int y)
@@ -49,6 +67,31 @@ namespace DynamicParser
             if (x >= Width || y >= Height)
                 throw new ArgumentException();
             return _points[Width * y + x];
+        }
+
+        public Line? GetLine(SignValue diff)
+        {
+            if (_lastY >= Height)
+            {
+                _lastX = _lastY = 0;
+                return null;
+            }
+            Points pts = GetPixel(_lastX, _lastY);
+            Points? pt = null;
+            for (_lastX++; _lastX < Width; _lastX++)
+            {
+                pt = GetPixel(_lastX, _lastY);
+                if (pts.Sign - pt.Value.Sign > diff)
+                    pt = new Points { Sign = pts.Sign - pt.Value.Sign, Pt = pt.Value.Pt };
+            }
+            if (_lastX >= Width)
+            {
+                _lastX = 0;
+                _lastY++;
+            }
+            if (pt == null)
+                throw new Exception();
+            return new Line(pts, pt.Value);
         }
     }
 }

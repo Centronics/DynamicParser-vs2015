4dc6332a32ba8dc500bd3d24c2511274beba4f8d
Илья <kras-05@mail.ru>
2016-10-14 00:31:17 +0300

    Начата разработка класса поиска объекта. Необходимо добавить метод для "выравнивания" краёв квадрата в классе ContextWidth.

--- DynamicParser/ContextCube.cs
+++ DynamicParser/ContextCube.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using DynamicProcessor;
 
 namespace DynamicParser
 {
-    class ContextCube
+    public class ContextCube
     {
-
+        public ContextCube(ContextLine source, List<ContextLine> find, SignValue diff)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+            if (find == null)
+                throw new ArgumentNullException();
+            if (find.Count <= 0)
+                throw new ArgumentException();
+            List<ObjectQuad> lstObj = new List<ObjectQuad>(ContextWidth.Objects(source, diff));
+            List<List<ObjectQuad>> lstFind = new List<List<ObjectQuad>>(find.Select(cl => new List<ObjectQuad>(ContextWidth.Objects(cl, diff))));
+        }
     }
 }

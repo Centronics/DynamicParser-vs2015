418811b9691002a1962dd2220b47943a763b56d8
Илья <kras-05@mail.ru>
2016-10-24 00:39:32 +0300

    Начата разработка нового движка с поддержкой цветов.

--- DynamicParser/Processor.cs
+++ DynamicParser/Processor.cs
@@ -1,160 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using DynamicProcessor;
+using System.Drawing;
 
 namespace DynamicParser
 {
-    public sealed class Data
+    public enum SignColor : byte
     {
-        static long _idCount;
-
-        readonly List<SignValue> _signList = new List<SignValue>();
-        readonly List<Data> _nextData = new List<Data>();
-        public long Id { get; private set; }
-
-        public Data()
-        {
-            IdInit();
-        }
-
-        public bool IsEmpty => (_signList?.Count ?? 0) <= 0;
-
-        public int Length => _signList?.Count ?? 0;
-
-        public void Clear()
-        {
-            _signList?.Clear();
-            _nextData?.Clear();
-            Id = -1;
-        }
-
-        IEnumerable<Data> NextData
-        {
-            get
-            {
-                yield return this;
-                foreach (Data dt in _nextData)
-                    yield return dt;
-            }
-        }
-
-        public Data(ICollection<SignValue> lstSv)
-        {
-            if (lstSv == null)
-                return;
-            if (lstSv.Count <= 0)
-                return;
-            _signList.AddRange(lstSv);
-        }
-
-        void IdInit()
-        {
-            if (_idCount == long.MaxValue)
-                throw new ArgumentException();
-            Id = Interlocked.Increment(ref _idCount);
-        }
-
-        bool InCase(IList<SignValue> lst)
-        {
-            if (lst == null)
-                return false;
-            if (lst.Count <= 0)
-                return false;
-            if (_signList.Count < lst.Count)
-                return false;
-            return !_signList.Where((t, k) => t != lst[k]).Any();
-        }
-
-        public Data ReadValues(long id)
-        {
-            return id < 0 ? null : NextData.FirstOrDefault(nd => (nd?.Id ?? -1) == Id);
-        }
-
-        public Data ReadValues(List<SignValue> lstSv)
-        {
-            return NextData.Any(nd => nd.InCase(lstSv)) ? this : null;
-        }
-
-        public Data ReadValues(Data data)
-        {
-            return (data?.IsEmpty ?? true) ? null : ReadValues(data._signList);
-        }
-
-        public Data WriteValues(Data data)
-        {
-            return (data?.IsEmpty ?? true) ? null : WriteValues(data._signList);
-        }
-
-        public void AddValues(Data data)
-        {
-            if (data?.IsEmpty ?? true)
-                return;
-            _nextData.Add(data);
-        }
-
-        public Data WriteValues(List<SignValue> lstSv)
-        {
-            if (lstSv == null)
-                return null;
-            if (lstSv.Count <= 0)
-                return null;
-            Data data = ReadValues(lstSv);
-            if (data != null) return data.WriteValues(lstSv);
-            if (_signList.Count <= 0)
-            {
-                _signList.AddRange(lstSv);
-                return this;
-            }
-            Data dt = new Data(lstSv);
-            _nextData.Add(dt);
-            return dt;
-        }
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        LightBlue,
+        Blue,
+        Violet
     }
 
-    public static class DataProcessor
+    public struct Information
     {
-        static IEnumerable<List<SignValue>> GetData(IList<SignValue> lstSv)
+        public SignColor CurrentSign { get; set; }
+        public List<SignColor> Maps { get; set; }
+    }
+
+    public sealed class Map
+    {
+        readonly SignColor[,] _bitmap;
+
+        public Map(Bitmap btm)
         {
-            if (lstSv == null || lstSv.Count <= 0)
-                yield break;
-            List<SignValue> lst = new List<SignValue>(lstSv);
-            for (int k = 0; k < lstSv.Count; k++)
+            if (btm == null)
+                throw new ArgumentNullException();
+            if (btm.Width <= 0 || btm.Height <= 0)
+                throw new ArgumentException();
+            _bitmap = new SignColor[btm.Width, btm.Height];
+            for (int y = 0; y < btm.Height; y++)
+                for (int x = 0; x < btm.Width; x++)
+                    _bitmap[x, y] = (SignColor)(unchecked((uint)btm.GetPixel(x, y).ToArgb()) / 2396745U);
+        }
+
+        public int Width => _bitmap.GetLength(0);
+
+        public int Height => _bitmap.GetLength(1);
+
+        public int Length => Width * Height;
+
+        public IEnumerable<Information[,]> GetMap(IList<Map> lstFunc)
+        {
+            foreach (Map func in lstFunc)
             {
-                lst.RemoveAt(0);
-                yield return lst;
-            }
-            for (int h = 0; h < lstSv.Count; h++)
-                for (int j = 1; j <= lstSv.Count; j++)
-                {
-                    List<SignValue> lst1 = new List<SignValue>(j);
-                    for (int k = h; k < lstSv.Count; k++)
+                if (func == null)
+                    throw new ArgumentNullException();
+                if (func.Length != Length)
+                    throw new ArgumentException();
+                Information[,] map = new Information[func.Width, func.Height];
+                for (int y = 0; y < func.Height; y++)
+                    for (int x = 0; x < func.Width; x++)
                     {
-                        if (lstSv.Count - k > j)
-                            break;
-                        lst1.Add(lstSv[k]);
+                        List<SignColor> lst = new List<SignColor>();
+                        if (map[x, y].Maps == null)
+                            map[x, y] = new Information { CurrentSign = _bitmap[x, y], Maps = lst };
+                        if (_bitmap[x, y] == func._bitmap[x, y])
+                            lst.Add(_bitmap[x, y]);
                     }
-                    yield return lst1;
-                }
-        }
-
-        public static Data ReadData(List<SignValue> dt, Data readData)
-        {
-            if (dt == null || dt.Count <= 0 || (readData?.IsEmpty ?? true)) return null;
-            Data data = readData.ReadValues(dt);
-            foreach (List<SignValue> lst in GetData(dt))
-                data.AddValues(readData.ReadValues(lst));
-            return data;
-        }
-
-        public static Data WriteData(Data dt, Data writeData)
-        {
-            if ((dt?.IsEmpty ?? true) || (writeData?.IsEmpty ?? true)) return null;
-            Data data = writeData.WriteValues(dt);
-            for (int k = 1; k < dt.Length; k++)
-                data.AddValues(writeData.WriteValues(dt));
-            return data;
+                yield return map;
+            }
         }
     }
 }

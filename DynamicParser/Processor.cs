14ea44fd2f1a9f6c6a98684036bd88128501a46c
Илья <kras-05@mail.ru>
2016-10-28 00:24:29 +0300

    Предложена новая модель работы.

--- DynamicParser/Processor.cs
+++ DynamicParser/Processor.cs
@@ -1,51 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
+using DynamicProcessor;
 
 namespace DynamicParser
 {
-    public enum SignColor : byte
+    public sealed class Processor
     {
-        Red,
-        Orange,
-        Yellow,
-        Green,
-        LightBlue,
-        Blue,
-        Violet,
-        Nothing = 8
-    }
+        readonly SignValue[,] _bitmap;
 
-    public struct Information
-    {
-        public SignColor CurrentSign { get; set; }
-        public List<SignColor> Maps { get; set; }
-    }
-
-    public sealed class Map
-    {
-        readonly SignColor[,] _bitmap;
-
-        public Map(int width, int height)
+        public Processor(int width, int height)
         {
             if (width <= 0)
                 throw new ArgumentException();
             if (height <= 0)
                 throw new ArgumentException();
-            _bitmap = new SignColor[width, height];
+            _bitmap = new SignValue[width, height];
         }
 
-        public Map(Bitmap btm)
+        public Processor(Bitmap btm)
         {
             if (btm == null)
                 throw new ArgumentNullException();
             if (btm.Width <= 0 || btm.Height <= 0)
                 throw new ArgumentException();
-            _bitmap = new SignColor[btm.Width, btm.Height];
+            _bitmap = new SignValue[btm.Width, btm.Height];
             for (int y = 0; y < btm.Height; y++)
                 for (int x = 0; x < btm.Width; x++)
-                    _bitmap[x, y] = (SignColor)(unchecked((uint)btm.GetPixel(x, y).ToArgb()) / 2396745U);
+                    _bitmap[x, y] = new SignValue(btm.GetPixel(x, y).ToArgb() / 2396745);
         }
 
         public int Width => _bitmap.GetLength(0);
@@ -53,57 +34,5 @@ namespace DynamicParser
         public int Height => _bitmap.GetLength(1);
 
         public int Length => Width * Height;
-
-        public IEnumerable<Map> GetSymbols(IEnumerable<Map> lstFunc)
-        {
-            if (lstFunc == null)
-                throw new ArgumentNullException();
-            IEnumerable<Map> enumerable = lstFunc as IList<Map> ?? lstFunc.ToList();
-            if (!IsEqual(enumerable))
-                throw new ArgumentException();
-            foreach (Map map in enumerable)
-            {
-                Map operations = new Map(map.Width, map.Height);
-                for (int y = 0; y < map.Height; y++)
-                    for (int x = 0; x < map.Width; x++)
-                    {
-                        SignColor sc = _bitmap[x, y];
-                        operations._bitmap[x, y] = map._bitmap[x, y] == sc
-                            ? SignColor.Nothing
-                            : sc;
-                    }
-                yield return operations;
-            }
-        }
-
-        bool IsEqual(IEnumerable<Map> lstFunc)
-        {
-            if (lstFunc == null)
-                return false;
-            int len = Length;
-            return lstFunc.All(map => len == map?.Length);
-        }
-
-        public IEnumerable<Information[,]> GetMap(IList<Map> lstFunc)
-        {
-            foreach (Map func in lstFunc)
-            {
-                if (func == null)
-                    throw new ArgumentNullException();
-                if (func.Length != Length)
-                    throw new ArgumentException();
-                Information[,] map = new Information[func.Width, func.Height];
-                for (int y = 0; y < func.Height; y++)
-                    for (int x = 0; x < func.Width; x++)
-                    {
-                        List<SignColor> lst = new List<SignColor>();
-                        if (map[x, y].Maps == null)
-                            map[x, y] = new Information { CurrentSign = _bitmap[x, y], Maps = lst };
-                        if (_bitmap[x, y] == func._bitmap[x, y])
-                            lst.Add(_bitmap[x, y]);
-                    }
-                yield return map;
-            }
-        }
     }
 }

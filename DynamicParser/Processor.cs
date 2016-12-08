fd01f583fca8aa8d8b254f2a5219f34840fc191d
Илья <kras-05@mail.ru>
2016-12-09 00:44:59 +0300

    Добавлена документация.

--- DynamicParser/Processor.cs
+++ DynamicParser/Processor.cs
@@ -8,36 +8,76 @@ using DynamicProcessor;
 
 namespace DynamicParser
 {
+    /// <summary>
+    /// Процессор, выполняющий поиск соответствующих указанных карт на рабочем поле.
+    /// Работает по принципу поисковика наиболее подходящей карты для каждого места на рабочем поле.
+    /// Поиск производится по всему полю.
+    /// </summary>
     public sealed class Processor
     {
+        /// <summary>
+        /// Разница между процентами соответствия объектов, которая позволяет считать их одинаковыми.
+        /// </summary>
         const double DiffEqual = 0.01;
 
+        /// <summary>
+        /// Рабочее поле, на котором производится поиск требумых данных.
+        /// </summary>
         readonly SignValue[,] _bitmap;
 
+        /// <summary>
+        /// Название текущей карты.
+        /// </summary>
         public string Tag { get; }
 
+        /// <summary>
+        /// Ширина.
+        /// </summary>
         public int Width => _bitmap.GetLength(0);
 
+        /// <summary>
+        /// Высота.
+        /// </summary>
         public int Height => _bitmap.GetLength(1);
 
+        /// <summary>
+        /// Длина. Вычисляется как: Width * Height.
+        /// </summary>
         public int Length => Width * Height;
 
+        /// <summary>
+        /// Извлекает объект рабочего поля.
+        /// </summary>
+        /// <param name="x">Координата X.</param>
+        /// <param name="y">Координата Y.</param>
+        /// <returns></returns>
         public SignValue this[int x, int y] => _bitmap[x, y];
 
+        /// <summary>
+        /// Генерирует регион, равный по размеру текущему рабочему полю.
+        /// </summary>
         public Region CurrentRegion => new Region(Width, Height);
 
+        /// <summary>
+        /// Генерирует поле для поиска обозначенных регионов на текущем рабочем поле.
+        /// </summary>
         public Attacher CurrentAttacher => new Attacher(Width, Height);
 
+        /// <summary>
+        /// Предназначен для загрузки изображения как рабочего поля.
+        /// </summary>
+        /// <param name="btm">Загружаемое изображение.</param>
+        /// <param name="tag">Название изображения.</param>
         public Processor(Bitmap btm, string tag)
         {
             if (btm == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(btm), $"{nameof(Processor)}: Изображение не может быть равно null.");
             if (btm.Width <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(Processor)}: Ширина изображения не может быть меньше или равна нулю ({btm.Width}).", nameof(btm));
             if (btm.Height <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(Processor)}: Высота изображения не может быть меньше или равна нулю ({btm.Height}).", nameof(btm));
             if (tag == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(tag), $"{nameof(Processor)}: {nameof(tag)} не может быть равен null.");
             _bitmap = new SignValue[btm.Width, btm.Height];
             for (int y = 0; y < btm.Height; y++)
                 for (int x = 0; x < btm.Width; x++)
@@ -45,14 +85,26 @@ namespace DynamicParser
             Tag = tag;
         }
 
+        /// <summary>
+        /// Подтверждает, является ли карта с указанным индексом наиболее подходящей в данной точке.
+        /// </summary>
+        /// <param name="db">Массив карт для поиска.</param>
+        /// <param name="x">Координата X.</param>
+        /// <param name="y">Координата Y.</param>
+        /// <param name="number">Индекс карты, которую требуется проверить.</param>
+        /// <returns>Возвращает true в случае, если number входит в список наиболее подходящих карт.</returns>
         static bool GetMinIndex(IDictionary<int, SignValue[,]> db, int x, int y, int number)
         {
-            if (x < 0 || y < 0 || number < 0)
-                throw new ArgumentException();
+            if (x < 0)
+                throw new ArgumentException($"{nameof(GetMinIndex)}: Координата x меньше нуля ({x}).", nameof(x));
+            if (y < 0)
+                throw new ArgumentException($"{nameof(GetMinIndex)}: Координата y меньше нуля ({y}).", nameof(y));
+            if (number < 0)
+                throw new ArgumentException($"{nameof(GetMinIndex)}: Индекс проверяемой карты меньше нуля ({number}).", nameof(number));
             if (db == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(db), $"{nameof(GetMinIndex)}: Массив карт для поиска равен null.");
             if (db.Count <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(GetMinIndex)}: Длина массива карт для поиска равна нулю ({db.Count}).", nameof(db));
             SignValue ind = SignValue.MaxValue;
             int n = -1;
             foreach (int key in db.Keys)
@@ -62,13 +114,20 @@ namespace DynamicParser
                 ind = mas[x, y];
                 n = key;
             }
-            return n >= 0 && db.Keys.Where(key => ind == db[key][x, y]).ToList().Contains(number);
+            return n >= 0 && db.Keys.Where(key => ind == db[key][x, y]).ToArray().Contains(number);
         }
 
+        /// <summary>
+        /// Находит все карты, которые имеют тот же процент соответствия, что и заданный.
+        /// </summary>
+        /// <param name="lst">Список процентного соответствия карт.</param>
+        /// <param name="perc">Искомый процент соответствия.</param>
+        /// <returns>Возвращает индексы карт, которые имеют тот же процент соответствия.
+        /// Индексы соответствуют поисковому массиву.</returns>
         static IEnumerable<int> GetMaxIndex(IList<double> lst, double perc)
         {
             if (lst == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(lst), $"{nameof(GetMaxIndex)}: Список процентного соответствия карт равен null.");
             if (lst.Count <= 0)
                 yield break;
             for (int k = 0; k < lst.Count; k++)
@@ -76,20 +135,34 @@ namespace DynamicParser
                     yield return k;
         }
 
+        /// <summary>
+        /// Находит соответствие между заданными картами и рабочей областью, производя поиск по ней.
+        /// </summary>
+        /// <param name="first">Первая карта, которая определяет размеры последующих. Не может быть null. Длина не может быть 0.</param>
+        /// <param name="processors">Все остальные карты. Может быть null.</param>
+        /// <returns>Возвращает SearchResults, содержащий поле с результатами поиска.</returns>
         public SearchResults GetEqual(Processor first, params Processor[] processors)
         {
             return GetEqual(new ProcessorContainer(first, processors));
         }
 
+        /// <summary>
+        /// Производит поиск указанных карт на рабочей области и возвращает результат в виде класса SearchResults,
+        /// содержащего поле с результатами поиска.
+        /// </summary>
+        /// <param name="prc">Массив карт для поиска.</param>
+        /// <returns>Возвращает SearchResults, содержащий поле с результатами поиска.</returns>
         public SearchResults GetEqual(ProcessorContainer prc)
         {
             if (prc == null)
-                throw new ArgumentNullException();
-            if (prc.Width > Width || prc.Height > Height)
-                throw new ArgumentException();
+                throw new ArgumentNullException(nameof(prc), $"{nameof(GetEqual)}: Массив карт для поиска равен null.");
+            if (prc.Width > Width)
+                throw new ArgumentException($"{nameof(GetEqual)}: Карты для поиска превышают существующее поле по ширине ({prc.Width} > {Width}).", nameof(prc));
+            if (prc.Height > Height)
+                throw new ArgumentException($"{nameof(GetEqual)}: Карты для поиска превышают существующее поле по высоте ({prc.Height} > {Height}).", nameof(prc));
             if (prc.Count <= 0)
-                throw new ArgumentException();
-            SearchResults sr = new SearchResults(Width, Height);//можно оптимизировать память, уменьшив размер результирующей карты
+                throw new ArgumentException($"{nameof(GetEqual)}: Массив карт для поиска ничего не содержит.", nameof(prc));
+            SearchResults sr = new SearchResults(Width, Height);
             string errString = string.Empty;
             Parallel.For(0, Height, y1 =>
             {
--- DynamicParser/Region.cs
+++ DynamicParser/Region.cs
@@ -5,112 +5,219 @@ using System.Linq;
 
 namespace DynamicParser
 {
+    /// <summary>
+    /// Представляет область, которая не пересекается ни с какой другой.
+    /// Хранит список наиболее соответствующих найденных карт.
+    /// </summary>
     public sealed class Registered
     {
+        /// <summary>
+        /// Область, которая не пересекается ни с какой другой.
+        /// </summary>
         public Rectangle Region { get; set; }
+
+        /// <summary>
+        /// Список наиболее соответствующих найденных карт.
+        /// </summary>
         public List<Reg> Register { get; set; }
 
+        /// <summary>
+        /// Возвращает true в случае, если список найденных карт пуст.
+        /// </summary>
         public bool IsEmpty => (Register?.Count ?? 0) <= 0;
 
+        /// <summary>
+        /// Координата X начала области.
+        /// </summary>
         public int X => Region.X;
 
+        /// <summary>
+        /// Координата Y начала области.
+        /// </summary>
         public int Y => Region.Y;
 
+        /// <summary>
+        /// Возвращает координату, являющуюся суммой свойств X и Width.
+        /// </summary>
         public int Right => Region.Right;
 
+        /// <summary>
+        /// Возвращает координату, являющуюся суммой свойств Y и Height.
+        /// </summary>
         public int Bottom => Region.Bottom;
 
+        /// <summary>
+        /// Выясняет, перекрывается ли текущая область с указанной.
+        /// </summary>
+        /// <param name="rect">Проверяемая область.</param>
+        /// <returns>Возвращает true в случае обнаружения перекрытия.</returns>
         public bool IsConflict(Rectangle rect)
         {
-            if (rect.X >= X && rect.Right <= Right)
+            return IsConflict(rect, Region);
+        }
+
+        /// <summary>
+        /// Выясняет, перекрывается ли текущая область с указанной.
+        /// </summary>
+        /// <param name="rect1">Проверяемая область.</param>
+        /// <param name="rect2">Проверяемая область.</param>
+        /// <returns>Возвращает true в случае обнаружения перекрытия.</returns>
+        public static bool IsConflict(Rectangle rect1, Rectangle rect2)
+        {
+            if (rect1.X >= rect2.X && rect1.Right <= rect2.Right)
                 return true;
-            if (rect.Y >= Y && rect.Bottom <= Bottom)
+            if (rect1.Y >= rect2.Y && rect1.Bottom <= rect2.Bottom)
                 return true;
-            if (rect.Right >= X && rect.Right <= Right)
+            if (rect1.Right >= rect2.X && rect1.Right <= rect2.Right)
                 return true;
-            if (rect.Bottom >= Y && rect.Bottom <= Bottom)
+            if (rect1.Bottom >= rect2.Y && rect1.Bottom <= rect2.Bottom)
                 return true;
-            if (rect.X <= X && rect.Right >= Right)
+            if (rect1.X <= rect2.X && rect1.Right >= rect2.Right)
                 return true;
-            return rect.Y <= Y && rect.Bottom >= Bottom;
+            return rect1.Y <= rect2.Y && rect1.Bottom >= rect2.Bottom;
         }
     }
 
+    /// <summary>
+    /// Представляет регион, разделённый на области, ни одна из которых не пересекается ни с какой другой.
+    /// </summary>
     public sealed class Region
     {
+        /// <summary>
+        /// Хранит области, индексируя их как Width * y + x.
+        /// </summary>
         readonly SortedDictionary<ulong, Registered> _rects = new SortedDictionary<ulong, Registered>();
 
+        /// <summary>
+        /// Ширина региона.
+        /// </summary>
         public int Width { get; }
 
+        /// <summary>
+        /// Высота региона.
+        /// </summary>
         public int Height { get; }
 
+        /// <summary>
+        /// Получает коллекцию хранимых областей.
+        /// </summary>
         public IEnumerable<Registered> Elements => _rects.Values;
 
+        /// <summary>
+        /// Получает коллекцию хранимых координат областей.
+        /// </summary>
         public IEnumerable<Rectangle> Rectangles => _rects.Values.Select(reg => reg.Region);
 
+        /// <summary>
+        /// Получает область, на которую попадает указанная точка или null, если она не попадает ни на какую.
+        /// </summary>
+        /// <param name="pt">Заданная точка.</param>
+        /// <returns>Возвращает область, на которую попадает указанная точка или null, если она не попадает ни на какую.</returns>
         public Registered this[Point pt] => this[pt.X, pt.Y];
 
+        /// <summary>
+        /// Инициализирует регион с указанными параметрами ширины и высоты.
+        /// </summary>
+        /// <param name="width">Ширина региона.</param>
+        /// <param name="height">Высота региона.</param>
         public Region(int width, int height)
         {
             if (width <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(Region)}: Ширина не может быть меньше или равна нулю ({width}).", nameof(width));
             if (height <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(Region)}: Высота не может быть меньше или равна нулю ({height}).", nameof(height));
             Width = width;
             Height = height;
         }
 
+        /// <summary>
+        /// Вычисляет индекс по формуле Width * y + x.
+        /// </summary>
+        /// <param name="x">Координата X.</param>
+        /// <param name="y">Координата Y.</param>
+        /// <returns>Возвращает индекс по формуле Width * y + x.</returns>
         ulong GetIndex(int x, int y)
         {
             if (x < 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(GetIndex)}: Координата X не может быть меньше нуля ({x}).", nameof(x));
             if (y < 0)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(GetIndex)}: Координата Y не может быть меньше нуля ({y}).", nameof(y));
             if (x >= Width)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(GetIndex)}: Координата X не может быть больше или равна ширине ({x}).", nameof(x));
             if (y >= Height)
-                throw new ArgumentException();
+                throw new ArgumentException($"{nameof(GetIndex)}: Координата Y не может быть больше или равна высоте ({y}).", nameof(y));
             return Convert.ToUInt64(Width * y + x);
         }
 
+        /// <summary>
+        /// Получает область, на которую попадает указанная точка или null, если она не попадает ни на какую.
+        /// </summary>
+        /// <param name="x">Координата X.</param>
+        /// <param name="y">Координата Y.</param>
+        /// <returns>Возвращает область, на которую попадает указанная точка или null, если она не попадает ни на какую.</returns>
         public Registered this[int x, int y]
         {
             get
             {
                 if (x < 0)
-                    throw new ArgumentException();
+                    throw new ArgumentException($"Indexer: Координата X не может быть меньше нуля ({x}).", nameof(x));
                 if (y < 0)
-                    throw new ArgumentException();
+                    throw new ArgumentException($"Indexer: Координата Y не может быть меньше нуля ({y}).", nameof(y));
+                if (x >= Width)
+                    throw new ArgumentException($"Indexer: Координата X не может быть больше или равна ширине ({x}).", nameof(x));
+                if (y >= Height)
+                    throw new ArgumentException($"Indexer: Координата Y не может быть больше или равна высоте ({y}).", nameof(y));
                 return Elements.FirstOrDefault(reg => x >= reg.X && x <= reg.Right && y >= reg.Y && y <= reg.Bottom);
             }
         }
 
+        /// <summary>
+        /// Заполняет объект Attacher информацией об областях, на которые попадают указанные в нём точки.
+        /// </summary>
+        /// <param name="attacher">Объект, содержащий данные об искомых точках.</param>
         public void SetMask(Attacher attacher)
         {
             if (attacher == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(attacher), $"{nameof(SetMask)}: Объект, содержащий информацию об искомых точках, не может быть равен null.");
             if (attacher.Width <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException(
+                    $"{nameof(SetMask)}: Ширина объекта, содержащего информацию о точках, не может быть меньше или равна нулю ({attacher.Width}).", nameof(attacher));
             if (attacher.Height <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException(
+                    $"{nameof(SetMask)}: Высота объекта, содержащего информацию о точках, не может быть меньше или равна нулю ({attacher.Height}).", nameof(attacher));
             foreach (Attach attach in attacher.Attaches)
                 attach.Regs = this[attach.Point].Register;
         }
 
+        /// <summary>
+        /// Определяет, приходится ли начало какой-либо области по указанным координатам.
+        /// </summary>
+        /// <param name="x">Координата X.</param>
+        /// <param name="y">Координата Y.</param>
+        /// <returns>Возвращает true в случае, если начало области приходится на заданные координаты, false в противном случае.</returns>
         public bool Contains(int x, int y)
         {
             return _rects.ContainsKey(GetIndex(x, y));
         }
 
+        /// <summary>
+        /// Определяет, перекрывается ли указанная область с какой-либо из содержащихся в объекте.
+        /// </summary>
+        /// <param name="rect">Проверяемая область.</param>
+        /// <returns>В случае, если перекрываются, возвращает true, в противном случае false.</returns>
         public bool IsConflict(Rectangle rect)
         {
             return _rects.Values.Any(reg => reg.IsConflict(rect));
         }
 
+        /// <summary>
+        /// Вставляет указанную область в коллекцию.
+        /// </summary>
+        /// <param name="rect">Вставляемая область.</param>
         public void Add(Rectangle rect)
         {
             if (IsConflict(rect))
-                throw new ArgumentException($"{nameof(Add)}: Попытка вставить элемент, конфликтующий с существующими");
+                throw new ArgumentException($"{nameof(Add)}: Попытка вставить элемент, конфликтующий с существующими.", nameof(rect));
             _rects[GetIndex(rect.X, rect.Y)] = new Registered { Region = rect };
         }
     }

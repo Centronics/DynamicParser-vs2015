eb5fe9af1e5a0e745bcc9bcf04c27363942419ad
Илья <kras-05@mail.ru>
2017-02-02 22:31:24 +0300

    Отлажен тест класса WordSearcher.

--- DynamicParserTest/WordSearcherTest.cs
+++ DynamicParserTest/WordSearcherTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DynamicParser;
 
@@ -11,52 +11,62 @@ namespace DynamicParserTest
         public void WordSearchTest()
         {
             {
-                WordSearcher ws = new WordSearcher(new[] { new List<string> { "0" }, new List<string> { "0", "1" }, new List<string> { "0", "1", "2" } });
+                WordSearcher ws = new WordSearcher(new[] { "0", "0", "1", "0", "1", "2" });
                 Assert.AreEqual(true, ws.IsEqual("000"));
                 Assert.AreEqual(true, ws.IsEqual("001"));
                 Assert.AreEqual(true, ws.IsEqual("002"));
                 Assert.AreEqual(true, ws.IsEqual("010"));
                 Assert.AreEqual(true, ws.IsEqual("011"));
                 Assert.AreEqual(true, ws.IsEqual("012"));
+                Assert.AreEqual(true, ws.IsEqual("012"));
+                Assert.AreEqual(true, ws.IsEqual("11111222"));
+                Assert.AreEqual(true, ws.IsEqual("111112220"));
+                Assert.AreEqual(true, ws.IsEqual("1100111222"));
+                Assert.AreEqual(true, ws.IsEqual("012"));
+                Assert.AreEqual(true, ws.IsEqual("021"));
+                Assert.AreEqual(true, ws.IsEqual("210"));
+                Assert.AreEqual(true, ws.IsEqual("102"));
+                Assert.AreEqual(true, ws.IsEqual("000"));
+                Assert.AreEqual(true, ws.IsEqual("111"));
+                Assert.AreEqual(true, ws.IsEqual("222"));
+                Assert.AreEqual(true, ws.IsEqual("22"));
+                Assert.AreEqual(true, ws.IsEqual("11"));
+                Assert.AreEqual(true, ws.IsEqual("01"));
+                Assert.AreEqual(true, ws.IsEqual("10"));
+                Assert.AreEqual(true, ws.IsEqual("02"));
+                Assert.AreEqual(true, ws.IsEqual("20"));
+                Assert.AreEqual(true, ws.IsEqual("0"));
+                Assert.AreEqual(true, ws.IsEqual("1"));
+                Assert.AreEqual(true, ws.IsEqual("2"));
 
                 Assert.AreEqual(false, ws.IsEqual("030"));
-                Assert.AreEqual(false, ws.IsEqual("014"));
-                Assert.AreEqual(true, ws.IsEqual("112"));
-            }
-
-            {
-                WordSearcher ws = new WordSearcher(new[] { new List<string> { "0", "4" }, new List<string> { "3", "5", "9" } });
-                Assert.AreEqual(true, ws.IsEqual("03"));
-                Assert.AreEqual(true, ws.IsEqual("05"));
-                Assert.AreEqual(true, ws.IsEqual("09"));
-                Assert.AreEqual(true, ws.IsEqual("43"));
-                Assert.AreEqual(true, ws.IsEqual("45"));
-                Assert.AreEqual(true, ws.IsEqual("49"));
-
-                Assert.AreEqual(true, ws.IsEqual("30"));
-                Assert.AreEqual(true, ws.IsEqual("50"));
-                Assert.AreEqual(true, ws.IsEqual("90"));
-                Assert.AreEqual(true, ws.IsEqual("34"));
-                Assert.AreEqual(true, ws.IsEqual("54"));
-                Assert.AreEqual(true, ws.IsEqual("94"));
-
-                Assert.AreEqual(true, ws.IsEqual("99"));
-                Assert.AreEqual(false, ws.IsEqual("88"));
-                Assert.AreEqual(false, ws.IsEqual("74"));
-            }
-
-            {
-                WordSearcher ws = new WordSearcher(new[] { new List<string> { "0", "5", "9", "7", "8" } });
-                Assert.AreEqual(true, ws.IsEqual("0"));
-                Assert.AreEqual(true, ws.IsEqual("5"));
-                Assert.AreEqual(true, ws.IsEqual("9"));
-                Assert.AreEqual(true, ws.IsEqual("8"));
-                Assert.AreEqual(true, ws.IsEqual("7"));
-
-                Assert.AreEqual(false, ws.IsEqual("4"));
+                Assert.AreEqual(false, ws.IsEqual("0141"));
                 Assert.AreEqual(false, ws.IsEqual("3"));
-                Assert.AreEqual(false, ws.IsEqual("2"));
+                Assert.AreEqual(false, ws.IsEqual(string.Empty));
+                Assert.AreEqual(false, ws.IsEqual(null));
             }
+
+            {
+                WordSearcher ws = new WordSearcher(new[] { "0", "0", "1", "0", "1", "2" });
+                Assert.AreEqual(false, ws.IsEqual(string.Empty));
+                Assert.AreEqual(false, ws.IsEqual(null));
+            }
+
+            {
+                WordSearcher ws = new WordSearcher(new[] { string.Empty, null, string.Empty, null });
+                Assert.AreEqual(false, ws.IsEqual(string.Empty));
+                Assert.AreEqual(false, ws.IsEqual(null));
+                Assert.AreEqual(false, ws.IsEqual("3"));
+                Assert.AreEqual(false, ws.IsEqual("20"));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void WordSearchException()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            new WordSearcher(null);
         }
     }
 }

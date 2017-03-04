647f7d317d0d6fc7bfc12460257a6ba52b209f1c
Илья <kras-05@mail.ru>
2017-03-04 13:47:11 +0300

    Исправление информации о сборке.

--- DynamicParser/Properties/AssemblyInfo.cs
+++ DynamicParser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Home")]
 [assembly: AssemblyProduct("DynamicParser")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
